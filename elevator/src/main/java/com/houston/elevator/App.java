c7231b952d751e5c3abd819ad9369d41092f740c
Juhani Hietikko <juhani.hietikko@houston-inc.com>
2012-07-06 10:47:16 +0300

    An empty mvn project set up.

---
+++ elevator/src/main/java/com/houston/elevator/App.java
@@ -0,0 +1,13 @@
+package com.houston.elevator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
