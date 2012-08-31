221661b42b011c5303643b1c0614051fb265ed15
Juhani Hietikko <juhani.hietikko@houston-inc.com>
2012-08-31 19:39:20 +0300

    Work done at the dojo.

--- elevator/src/main/java/com/houston/elevator/controller/ElevatorControllerImpl.java
+++ elevator/src/main/java/com/houston/elevator/controller/ElevatorControllerImpl.java
@@ -2,21 +2,59 @@ package com.houston.elevator.controller;
 
 import java.util.List;
 
+import com.houston.elevator.api.ControlPanelInput;
 import com.houston.elevator.api.Elevator;
 import com.houston.elevator.api.ElevatorController;
-import com.houston.elevator.api.ControlPanelInput;
+import com.houston.elevator.api.ElevatorStatus;
+import com.houston.elevator.api.ElevatorStatus.KinematicStatus;
 import com.houston.elevator.api.Order;
 
 public class ElevatorControllerImpl implements ElevatorController {
 
-    public void registerElevator(Elevator elevator) {
-        // TODO Auto-generated method stub
+    private Elevator elevator;
 
+    public void registerElevator(Elevator elevator) {
+        this.elevator = elevator;
     }
 
-    public void processInput(List<Order> queuedOrders, List<ControlPanelInput> queuedPanelInput) {
-        // TODO Auto-generated method stub
-        
+    public void processInput(List<Order> queuedOrders,
+            List<ControlPanelInput> queuedPanelInput) {
+
+        ElevatorStatus status = elevator.getStatus();
+//        if (status != null
+//                && status.getKinematicStatus().equals(
+//                        KinematicStatus.STATIONARY)) {
+//            elevator.openDoors();
+//        }
+//
+        processPanelInputs(queuedPanelInput);
+//        processOrders(queuedOrders);
+    }
+
+    public void processOrders(List<Order> queuedOrders) {
+
+        if (queuedOrders != null) {
+            elevator.goToFloor(queuedOrders.get(0).getFloor());
+        }
+    }
+
+    public void processPanelInputs(List<ControlPanelInput> queuedPanelInput) {
+        if (queuedPanelInput != null) {
+            ControlPanelInput controlPanelInput = queuedPanelInput.get(0);
+
+//            switch (controlPanelInput.getType()) {
+//            case GO_TO_FLOOR:
+//                elevator.goToFloor(controlPanelInput.getSelectedFloor());
+//                break;
+//            case OPEN_DOORS:
+//                elevator.openDoors();
+//                break;
+//            default:
+//                elevator.closeDoors();
+//            }
+
+        }
+
     }
 
 }
--- elevator/src/test/java/com/houston/elevator/controller/ElevatorControllerImplTest.java
+++ elevator/src/test/java/com/houston/elevator/controller/ElevatorControllerImplTest.java
@@ -1,11 +1,103 @@
 package com.houston.elevator.controller;
 
+import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.houston.elevator.api.ControlPanelInput;
+import com.houston.elevator.api.ControlPanelInput.InputType;
+import com.houston.elevator.api.Elevator;
+import com.houston.elevator.api.ElevatorStatus;
+import com.houston.elevator.api.Order;
+import com.houston.elevator.api.Order.OrderType;
 
 public class ElevatorControllerImplTest {
 
-    @Test
-    public void todoFirstTest() {
+    private Elevator elevator;
+    private ElevatorControllerImpl controller;
+
+    @Before
+    public void setUp() {
+        elevator = Mockito.mock(Elevator.class);
+        controller = new ElevatorControllerImpl();
+        controller.registerElevator(elevator);
     }
+
+    @Test
+    public void shouldGoToFirstFloor() {
+        processInput(InputType.GO_TO_FLOOR, 1);
+
+        Mockito.verify(elevator).goToFloor(1);
+    }
+
+    @Test
+    public void shouldGoToSecondFloor() {
+        processInput(InputType.GO_TO_FLOOR, 2);
+        Mockito.verify(elevator).goToFloor(2);
+    }
+
+    @Test
+    public void openDoors() {
+        processInput(InputType.OPEN_DOORS, 1);
+
+        Mockito.verify(elevator).openDoors();
+    }
+
+    @Test
+    public void closeDoors() {
+        processInput(InputType.CLOSE_DOORS, 1);
+        Mockito.verify(elevator).closeDoors();
+    }
+
+    @Test
+    public void shouldMoveElevatorToFirstFloorWhenOrdered() {
+        processOrder(1, OrderType.GOING_DOWN);
+        Mockito.verify(elevator).goToFloor(1);
+    }
+
+    @Test
+    public void shouldMoveElevatorToThirdFloorWhenOrdered() {
+        processOrder(3, OrderType.GOING_DOWN);
+        Mockito.verify(elevator).goToFloor(3);
+    }
+
+    @Test
+    public void shouldOpenDoorsWhenOrderedAndInTheOrderingFloor() {
+        processOrder(3, OrderType.GOING_DOWN);
+
+        Mockito.verify(elevator, Mockito.never()).openDoors();
+
+        ElevatorStatus isArrived = new ElevatorStatus(2, 2,
+                ElevatorStatus.DoorStatus.CLOSED);
+        Mockito.when(elevator.getStatus()).thenReturn(isArrived);
+
+        controller.processInput(null, null);
+        Mockito.verify(elevator).openDoors();
+    }
+
+    @Test
+    public void shouldNotOpenDoorsWhenOrderedAndNotInTheOrderingFloor() {
+        ElevatorStatus isArrived = new ElevatorStatus(1, 2,
+                ElevatorStatus.DoorStatus.CLOSED);
+        Mockito.when(elevator.getStatus()).thenReturn(isArrived);
+
+        processOrder(3, OrderType.GOING_DOWN);
+
+        Mockito.verify(elevator, Mockito.never()).openDoors();
+        controller.processInput(null, null);
+        Mockito.verify(elevator, Mockito.never()).openDoors();
+    }
+
+    private void processOrder(int floor, OrderType type) {
+        Order order = new Order(floor, type);
+        controller.processInput(Arrays.asList(order), null);
+    }
+
+    private void processInput(InputType type, int floor) {
+        ControlPanelInput input = new ControlPanelInput(elevator, type, floor);
+        controller.processInput(null, Arrays.asList(input));
+    }
+
 }
